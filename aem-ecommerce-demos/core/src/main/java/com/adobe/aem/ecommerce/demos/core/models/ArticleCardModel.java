e38425fff7919d323eef7bf5f6a007900b277d2c
pankaj_pwc <pankajkateja515@gmail.com>
2024-12-18 18:34:42 +0530

    Updated the page, Article-card component

--- aem-ecommerce-demos/core/src/main/java/com/adobe/aem/ecommerce/demos/core/models/ArticleCardModel.java
+++ aem-ecommerce-demos/core/src/main/java/com/adobe/aem/ecommerce/demos/core/models/ArticleCardModel.java
@@ -2,7 +2,6 @@ package com.adobe.aem.ecommerce.demos.core.models;
 
 import com.adobe.aem.ecommerce.demos.core.beans.ArticleCard;
 import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.PageManager;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang.StringUtils;
@@ -13,6 +12,7 @@ import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
@@ -24,7 +24,10 @@ import java.util.*;
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class ArticleCardModel {
 
-    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ArticleCardModel.class);
+    /**
+     * Logger LOG
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(ArticleCardModel.class);
 
     /**
      * RESOURCE_TYPE.
@@ -34,39 +37,48 @@ public class ArticleCardModel {
     @Inject
     private String articleRootPath;
 
+    @Inject
+    private String articleType;
+
     /**
      * resourceResolver
      */
     @SlingObject
     private ResourceResolver resourceResolver;
 
-    private String pageTitle;
-    private String pageDescription;
-    private String imagePathRef;
-
     private List<ArticleCard> articleCards;
+
     private Map<String, Object> propertyMap= new HashMap<>();
 
     private String jsonStringData;
 
     @PostConstruct
     protected void init() {
-        articleCards = new ArrayList<>();
+    articleCards = new ArrayList<>();
         Optional.ofNullable(resourceResolver.getResource(articleRootPath)).ifPresent(pageResource -> {
-            Page resourcePage = pageResource.adaptTo(Page.class);
-            Iterator<Page> childPages = resourcePage.listChildren();
-            while (childPages.hasNext()) {
-                Page childPage = childPages.next();
-                pageTitle = childPage.getProperties().get("jcr:title", StringUtils.EMPTY);
-                pageDescription = childPage.getProperties().get("jcr:description", StringUtils.EMPTY);
-                imagePathRef = getImageReference(childPage);
-                ArticleCard articleCard = new ArticleCard(pageTitle, pageDescription, imagePathRef);
-                articleCards.add(articleCard);
+        Page resourcePage = pageResource.adaptTo(Page.class);
+        Iterator<Page> childPages = resourcePage.listChildren();
+        while (childPages.hasNext()) {
+            Page childPage = childPages.next();
+
+            if ("articleCard".equals(articleType) || childPage.getProperties().get("showPageAsBanner", false)) {
+                processPage(childPage, articleCards);
             }
-        });
+        }
+    });
         propertyMap.put("article-cards", articleCards);
         jsonStringData = generateJsonString(propertyMap);
         LOG.info("Final Json Data=>> {}", jsonStringData);
+    }
+
+
+    private void processPage(Page childPage, List<ArticleCard> articleCards) {
+        String pageTitle = childPage.getProperties().get("jcr:title", StringUtils.EMPTY);
+        String pageDescription = childPage.getProperties().get("jcr:description", StringUtils.EMPTY);
+        String imagePathRef = getImageReference(childPage);
+        ArticleCard articleCard = new ArticleCard(pageTitle, pageDescription, imagePathRef);
+
+        articleCards.add(articleCard);
     }
 
     private String getImageReference(Page childPage) {
